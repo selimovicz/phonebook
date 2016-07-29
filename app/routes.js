f3475fd66921bb66ea6a5ea2e75ad8f88f6cf8fe
Zeid <selimoviczeid@gmail.com>
2016-07-29 19:26:52 +0200

    Protecting routes, access token checking

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,99 @@ var Book = require('./models/book');
 var User = require('./models/user'); 
 
 var jwt    = require('jsonwebtoken');
+var express  = require('express');
+
 module.exports = function(app) {
+
+	var apiRoutes = express.Router(); 
+
+	// route to authenticate a user
+	apiRoutes.post('/authenticate', function(req, res) {
+
+	  // find the user
+	  User.findOne({
+	    email: req.body.email
+	  }, function(err, user) {
+
+	    if (err) throw err;
+	    if (!user) {
+	      res.json({ success: false, message: 'Authentication failed. User not found.' });
+	    } else if (user) {
+	      // check if password matches
+	      if (user.password != req.body.password) {
+	        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+	      } else {
+
+	        // if user is found and password is right
+	        // create a token
+	        var token = jwt.sign(user, app.get('superSecret'), {
+	          expiresIn: 1440*3600 // expires in 24 hours
+	        });
+
+	        // return the information including token as JSON
+	        res.json({
+	          success: true,
+	          message: 'Enjoy your token!',
+	          token: token
+	        });
+	      }   
+
+	    }
+
+	  });
+	});
+
+	// create dummy user
+	apiRoutes.get('/setup', function(req, res) {
+
+	  // create a sample user
+	  var user = new User({ 
+	    email: 'test@test.com', 
+	    password: 'test123'
+	  });
+
+	  // save the sample user
+	  user.save(function(err) {
+	    if (err) throw err;
+	    res.json({ success: true });
+	  });
+
+	});
+
+	apiRoutes.use(function(req, res, next) {
+
+	  // check header or url parameters or post parameters for token
+	  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	  // decode token
+	  if (token) {
+
+	    // verifies secret and checks exp
+	    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+	      if (err) {
+	        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+	      } else {
+	        // if everything is good, save to request for use in other routes
+	        req.decoded = decoded;    
+	        next();
+	      }
+	    });
+
+	  } else {
+
+	    // if there is no token
+	    // return an error
+	    return res.status(403).send({ 
+	        success: false, 
+	        message: 'No token provided.' 
+	    });
+	    
+	  }
+	});
 
 	// api ---------------------------------------------------------------------
 	// get all books
-	app.get('/api/books', function(req, res) {
+	apiRoutes.get('/books', function(req, res) {
 
 		// use mongoose to get all locations in the database
 		Book.find(function(err, books) {
@@ -22,7 +110,7 @@ module.exports = function(app) {
 
 
 	// get single location
-	app.get('/api/books/:id', function(req, res) {
+	apiRoutes.get('/books/:id', function(req, res) {
 		// use mongoose to get single book in the database
 		Book.find({
 	 		_id : req.params.id
@@ -36,7 +124,7 @@ module.exports = function(app) {
 	});
 
 	// create entry and send back all books after creation
-	app.post('/api/books', function(req, res) {
+	apiRoutes.post('/books', function(req, res) {
 
 		// create a book, information comes from AJAX request from Angular
 		Book.create({
@@ -59,7 +147,7 @@ module.exports = function(app) {
 	});
 
 	// update book
-	app.put('/api/book/:book_id', function(req, res) {
+	apiRoutes.put('/book/:book_id', function(req, res) {
 	    Book.update({ _id: req.params.book_id }, req.body, function (err, books) {
 	        if (err){
 				res.send(err);
@@ -75,7 +163,7 @@ module.exports = function(app) {
 	});
 
 	// delete book
-	app.delete('/api/book/:book_id', function(req, res) {
+	apiRoutes.delete('/book/:book_id', function(req, res) {
 		Book.remove({
 			_id : req.params.book_id
 		}, function(err, books) {
@@ -91,68 +179,10 @@ module.exports = function(app) {
 		});
 	});
 
-
-	// user --------------------------------------------------------------------
-
-	// create dummy user
-	app.get('/api/setup', function(req, res) {
-
-	  // create a sample user
-	  var user = new User({ 
-	    email: 'test@test.com', 
-	    password: 'test123'
-	  });
-
-	  // save the sample user
-	  user.save(function(err) {
-	    if (err) throw err;
-	    console.log('User saved successfully');
-	    res.json({ success: true });
-	  });
-
-	});
-
-	// route to authenticate a user (POST http://localhost:8080/api/authenticate)
-	app.post('/api/authenticate', function(req, res) {
-
-	  // find the user
-	  User.findOne({
-	    name: req.body.email
-	  }, function(err, user) {
-
-	    if (err) throw err;
-	    if (!user) {
-	      res.json({ success: false, message: 'Authentication failed. User not found.' });
-	    } else if (user) {
-	    	console.log(user);
-	      // check if password matches
-	      if (user.password != req.body.password) {
-	        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-	      } else {
-
-	        // if user is found and password is right
-	        // create a token
-	        var token = jwt.sign(user, app.get('superSecret'), {
-	          expiresInMinutes: 1440 // expires in 24 hours
-	        });
-
-	        // return the information including token as JSON
-	        res.json({
-	          success: true,
-	          message: 'Enjoy your token!',
-	          token: token
-	        });
-	      }   
-
-	    }
-
-	  });
-	});
+	app.use('/api', apiRoutes);
 
 
-
-
-	// application -------------------------------------------------------------
+	// apiRouteslication -------------------------------------------------------------
 	app.get('*', function(req, res) {
 		res.sendfile('./public/app.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
